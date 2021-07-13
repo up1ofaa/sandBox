117ead8504020e875094d938effe0681f6894e7c
up1ofaa <81739397+up1ofaa@users.noreply.github.com>
2021-07-13 13:02:18 +0900

    useInputs

--- hooks/useInputsB.js
+++ hooks/useInputsB.js
@@ -1,7 +1,14 @@
 import { useState, useCallback } from "react";
 
-function useInputsB(initialForm){
-  const[form,setForm] =useState(initialForm);
+function useInputsB(initialForm) {
+  const [form, setForm] = useState(initialForm);
   //change
-  const onChange=useCallback(e=>)
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setForm((form) => ({ ...form, [name]: value }));
+  }, []);
+  const reset = useCallback(() => setForm(initialForm), [initialForm]);
+  return [form, onChange, reset];
 }
+
+export default useInputsB;
