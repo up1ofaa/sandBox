5ec476e4d6f028dd7b9449660ec5cc63cca74c27
up1ofaa <81739397+up1ofaa@users.noreply.github.com>
2021-07-13 13:24:32 +0900

    createUserB

---
+++ CreateUserB.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CreateUserB({}) {
+  return (
+    <div>
+      <input />
+      <input />
+      <button></button>
+    </div>
+  );
+}
+
+export default CreateUserB;
